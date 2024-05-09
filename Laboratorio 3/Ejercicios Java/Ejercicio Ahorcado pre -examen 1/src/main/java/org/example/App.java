5d174f3818db8df4313938820ef903234db09e9c
Mateo Bonansea <mateo.bonansea@gmail.com>
2024-05-09 00:06:45 -0300

    Corregido Ahorcado

--- Laboratorio 3/Ejercicios Java/Ejercicio Ahorcado pre -examen 1/src/main/java/org/example/App.java
+++ Laboratorio 3/Ejercicios Java/Ejercicio Ahorcado pre -examen 1/src/main/java/org/example/App.java
@@ -1,12 +1,11 @@
 package org.example;
-public class App 
-{
 
-    private static Juego juego = new Juego();
+public class App {
 
-    
-    public static void main( String[] args ) {
-        System.out.println("Hello, Practica Parcial 1 - Ahorcado.");
+    private static final Juego juego = new Juego();
+
+    public static void main(String[] args) {
+        System.out.println("Practica Parcial 1 - Ahorcado.");
         juego.bienvenida();
         juego.cargarJugador();
         Boolean playAgain = true;
@@ -18,7 +17,7 @@ public class App
                 if (!terminado) {
                     Character letra = juego.pedirLetra();
                     String l = letra.toString();
-                    juego.addLetra(letra);  
+                    juego.addLetra(letra);
                     terminado = juego.calcularEstadoDelJuego(l);
                 }
             } while (!terminado);
--- Laboratorio 3/Ejercicios Java/Ejercicio Ahorcado pre -examen 1/src/main/java/org/example/Juego.java
+++ Laboratorio 3/Ejercicios Java/Ejercicio Ahorcado pre -examen 1/src/main/java/org/example/Juego.java
@@ -1,58 +1,111 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Juego {
     private String palabraEnJuego;
     private String palabraModoOculto;
     private List<Character> letrasElegidas;
-    private Integer vidasJugador;
- private Scanner scanner;
+    private Integer vidasJugador = 5;
+    private Scanner scanner;
     private Jugador jugador;
+    private final List<String> palabras = Arrays.asList("abrir", "acero", "acto", "agua", "aire", "alma", "alta",
+            "amigo", "amor", "angel", "animal", "ansia", "apoyo", "arena", "arte", "asado", "asistir", "atajo", "atlas",
+            "juego", "jugoso", "julio", "junio", "juntar", "justo", "juzgar", "labio", "lado", "ladron", "laguna",
+            "lagrima", "lamentar", "lapiz", "largo", "leal", "leccion", "leche", "lector", "legumbre",
+            "lejos", "lengua", "leon", "letra", "levantar", "leyenda", "libertad", "libro", "licor", "lider", "lienzo",
+            "ligeramente", "limon", "lindo", "liquido", "listo", "llama", "llanto", "llegar", "lleno", "llevar",
+            "lluvia", "lobo", "locura", "lomo", "lote", "luchar", "luego", "lujo", "luna", "lunes", "lupa", "lustro",
+            "luz", "maceta", "madera", "maestro", "mafia", "magia", "mago", "maiz", "maldad", "maleta", "malla", "malo",
+            "mandar", "manejar", "manga", "mano", "mansion", "manta", "mapa", "maravilla", "marcar",
+            "marcha", "marfil", "marmol", "marron", "martes", "marzo", "masa", "mascara", "masivo", "matar", "maximo",
+            "mayo", "mayor", "mecha", "medalla", "medir", "meditar", "medula");
 
-
-    public Scanner getScanner() {
-        return scanner;
-    }
-
-    public void setScanner(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
-    public Jugador getJugador() {
-        return jugador;
-    }
-
-    public void setJugador(Jugador jugador) {
-        this.jugador = jugador;
+    public String getPalabra() {
+        Random random = new Random();
+        int indexRandom = random.nextInt(palabras.size());
+        return palabras.get(indexRandom);
     }
 
     public void bienvenida() {
-        
+
         System.out.println("Bienvenido al juego del AHORCADO!!!!!!!!!");
     }
 
     public void cargarJugador() {
-    
+
         System.out.println("Ingrese nombre del jugador por favor");
-        String nombre=scanner.nextLine();
-        jugador=new Jugador(nombre);
+        String nombre = scanner.nextLine();
+        jugador = new Jugador(nombre);
     }
 
+    public String getPalabraModoOculto() {
+        /*
+         * EXPLICACION
+         * El objeto StringBuilder:
+         * StringBuilder es una clase en Java que se utiliza para crear y manipular
+         * cadenas de caracteres
+         * que pueden ambiar (por ejemplo, cuando estás añadiendo, eliminando o
+         * cambiando caracteres en una cadena).
+         * Es especialmente útil cuando necesitas realizar muchas modificaciones en una
+         * cadena de texto,
+         * ya que es más eficiente en términos de rendimiento que manipular cadenas con
+         * la clase String de Java.
+         */
+        StringBuilder palabraOculta = new StringBuilder(palabraEnJuego.length());
+        // se mapea la palabra en juego y se compara con las letras elegidas
+        for (int i = 0; i < palabraEnJuego.length(); i++) {
+            // se obtiene el caracter en la posicion i
+            Character l = palabraEnJuego.charAt(i);
+            // si la letra elegida (l) contiene la letra en la posicion i se agrega la letra
+            // a
+            // la palabra oculta
+            // si no, se agrega un guion bajo, quiere decir que sigue oculta
+            // todos estos comentarios lo hice a mano para entender el codigo
+            if (letrasElegidas.contains(l)) {
+                palabraOculta.append(l);
+            } else {
+                palabraOculta.append("_");
+            }
+        }
+        setPalabraModoOculto(palabraOculta.toString());
+        return palabraOculta.toString();
+    }
+
+    public void dibujar() {
+        String palabraOculta = getPalabraModoOculto();
+
+
+        System.out.println("Palabra: " + palabraOculta);
+        System.out.println("Vidas restantes: " + vidasJugador);
+    }
+
+    private Boolean validarInputLetra(String input) {
+        return input.length() == 1;
+    }
+
+    private Character getCharacterFromInput(String input) {
+
+        char caracter;
+        if (input.length() == 1) {
+            caracter = input.charAt(0);
+        } else
+            return null;
+        return caracter;
+    }
 
     public Character pedirLetra() {
-
         Character letra = null;
         do {
             System.out.println("Ingrese una letra");
             String input = scanner.nextLine();
-            
-            if(validarInputLetra(input)) {
-                letra = getCharacterFromInput(input);
+
+            if (validarInputLetra(input)) {
+                if (palabraModoOculto.contains(input)){
+                    System.out.println("Ya ingresaste esa letra, por favor ingrese otra");
+                } else {
+                    letra = getCharacterFromInput(input);
+                }
             } else {
                 System.out.println("Error...Ingrese una letra valida");
             }
@@ -60,33 +113,12 @@ public class Juego {
         return letra;
     }
 
-    private Character getCharacterFromInput(String input) {
-    
-
-        char caracter;
-        if(input.length() == 1){
-            caracter = input.charAt(0);
-        }
-        else return null;
-        return caracter;
-    }
-
-    private Boolean validarInputLetra(String input) {
-        if(input.length()==1)
-        {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
     public Boolean getPlayAgain() {
-        Scanner scanner = new Scanner(System.in);
         boolean confirm = false;
-        
+
         while (true) {
             System.out.println("Desea seguir jugando?: (y/n) ");
-            
+
             String rta = scanner.nextLine();
             if (rta.toLowerCase().equals("y")) {
                 confirm = true;
@@ -98,46 +130,12 @@ public class Juego {
                 System.out.println("Error...Ingrese una respuesta valida");
             }
         }
-        
-        
+
         return confirm;
     }
+
     public void despedida() {
         scanner.close();
-    }
-    public String getPalabraEnJuego() {
-        return palabraEnJuego;
-    }
-
-    public void setPalabraEnJuego(String palabraEnJuego) {
-        this.palabraEnJuego = palabraEnJuego;
-    }
-
-    public void setPalabraModoOculto(String palabraModoOculto) {
-        this.palabraModoOculto = palabraModoOculto;
-    }
-
-    public List<Character> getLetrasElegidas() {
-        return letrasElegidas;
-    }
-
-    public void setLetrasElegidas(List<Character> letrasElegidas) {
-        this.letrasElegidas = letrasElegidas;
-    }
-
-    public Integer getVidasJugador() {
-        return vidasJugador;
-    }
-
-    public void setVidasJugador(Integer vidasJugador) {
-        this.vidasJugador = vidasJugador;
-    }
-
-    public Juego() {
-        this.palabraEnJuego = getPalabra();
-        this.letrasElegidas = new ArrayList<>();
-        this.vidasJugador = 6;
-        this.scanner = new Scanner(System.in);
     }
 
     public void addLetra(Character letra) {
@@ -146,67 +144,34 @@ public class Juego {
             this.palabraModoOculto = this.getPalabraModoOculto();
         }
     }
-public Boolean calcularEstadoDelJuego(String letra) {
-    
-    if (palabraEnJuego.contains(letra.toString())) {
-        if (palabraModoOculto.equals(palabraEnJuego)) {
-            System.out.println("¡Felicidades, ganaste el juego!");
-            return true;
-        }
-        return false;
-    } else {
-        vidasJugador--;
-        if (vidasJugador <= 0) {
-            System.out.println("El juego terminó, perdiste");
-            return true;
-        }
-        return false;
-    }
-}
-    
-    public String getPalabraModoOculto() {
-        StringBuilder palabraOculta = new StringBuilder(palabraEnJuego.length());
-        for (int i = 0; i < palabraEnJuego.length(); i++) {
-            Character l = palabraEnJuego.charAt(i);
-            if (letrasElegidas.contains(l)) {
-                palabraOculta.append(l);
-            } else { 
-                palabraOculta.append("_");
+
+    public Boolean calcularEstadoDelJuego(String letra) {
+
+        if (palabraEnJuego.contains(letra)) {
+            if (palabraModoOculto.equals(palabraEnJuego)) {
+                System.out.println("¡Felicidades, ganaste el juego!, La palabra correcta era: " + palabraEnJuego);;
+                return true;
             }
+            return false;
+        } else {
+            vidasJugador--;
+            if (vidasJugador <= 0) {
+                System.out.println("¡Perdiste! La palabra correcta era: " + palabraEnJuego);
+                return true;
+            }
+            return false;
         }
-        setPalabraModoOculto(palabraOculta.toString());
-        return palabraOculta.toString();
     }
 
-
-    public void dibujar() {
-        String palabraOculta = getPalabraModoOculto();
-
-        if (vidasJugador<=0) {
-            System.out.println("¡Perdiste! La palabra correcta era: " + palabraEnJuego);
-            return;
-        }
-
-        System.out.println("Palabra: " + palabraOculta);
-        System.out.println("Vidas restantes: " + vidasJugador);
+    public void setPalabraModoOculto(String palabraModoOculto) {
+        this.palabraModoOculto = palabraModoOculto;
     }
 
-      private List<String> palabras = Arrays.asList("abrir", "acero", "acto", "agua", "aire", "alma", "alta", "amigo",
-            "amor", "angel", "animal", "ansia", "apoyo", "arena", "arte", "asado", "asistir", "atajo", "atlas",
-            "juego", "jugoso", "julio", "junio", "juntar", "justo", "juzgar", "labio", "lado", "ladron", "laguna",
-            "lagrima", "lamentar", "lapiz", "largo", "leal", "leccion", "leche", "lector", "legumbre",
-            "lejos", "lengua", "leon", "letra", "levantar", "leyenda", "libertad", "libro", "licor", "lider", "lienzo",
-            "ligeramente", "limon", "lindo", "liquido", "listo", "llama", "llanto", "llegar", "lleno", "llevar",
-            "lluvia", "lobo", "locura", "lomo", "lote", "luchar", "luego", "lujo", "luna", "lunes", "lupa", "lustro",
-            "luz", "maceta", "madera", "maestro", "mafia", "magia", "mago", "maiz", "maldad", "maleta", "malla", "malo",
-             "mandar", "manejar", "manga", "mano", "mansion", "manta", "mapa", "maravilla", "marcar",
-            "marcha", "marfil", "marmol", "marron", "martes", "marzo", "masa", "mascara", "masivo", "matar", "maximo",
-            "mayo", "mayor", "mecha", "medalla", "medir", "meditar", "medula");
-
-
-    public String getPalabra() {
-        Random random = new Random();
-        int indexRandom = random.nextInt(palabras.size());
-        return palabras.get(indexRandom);
+    public Juego() {
+        this.palabraEnJuego = getPalabra();
+        this.letrasElegidas = new ArrayList<>();
+        this.vidasJugador = 5;
+        this.scanner = new Scanner(System.in);
     }
+
 }
