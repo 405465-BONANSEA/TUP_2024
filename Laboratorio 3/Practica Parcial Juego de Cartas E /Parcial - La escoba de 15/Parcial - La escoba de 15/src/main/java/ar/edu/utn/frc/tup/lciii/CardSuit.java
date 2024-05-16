479440b756c0461a5379291b496b5358f1efe17d
Mateo Bonansea <142741192+mateobonansea@users.noreply.github.com>
2024-05-16 14:42:28 -0300

    Delete Laboratorio 3/Practica Parcial Juego de Cartas E /Parcial - La escoba de 15/Parcial - La escoba de 15 directory

--- Laboratorio 3/Practica Parcial Juego de Cartas E /Parcial - La escoba de 15/Parcial - La escoba de 15/src/main/java/ar/edu/utn/frc/tup/lciii/CardSuit.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frc.tup.lciii;
-
-/**
- * Enumerador con todos los posibles palos de una baraja de cartas españolas
- */
-public enum CardSuit {
-    OROS, COPAS, ESPADAS, BASTOS
-}
